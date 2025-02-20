55f94ad87de75a296df2f901264a3f4167922269
lenguyengiabao <lenguyengiabao46@gmail.com>
2025-02-20 23:28:02 +0700

    fix(be): add app exception handler

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from app.infrastructure.database import init_db
+from app.infrastructure.response import ResponseFormatter
 from app.interfaces.controllers import router
-from fastapi import FastAPI
+from constants import FAILED
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
 
 app = FastAPI()
 init_db()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return ResponseFormatter.error(FAILED)
+
 
 app.include_router(router)
 
