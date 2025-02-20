c991ab501d258071d74f10dfa26d1aae37e3fe91
lenguyengiabao <lenguyengiabao46@gmail.com>
2025-02-21 00:03:49 +0700

    feat(be): update model

--- backend/app/domain/models.py
+++ backend/app/domain/models.py
@@ -1,13 +1,21 @@
+import uuid
 from datetime import datetime
 
 from app.infrastructure.database import Base
 from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class Record(Base):
-    __tablename__ = "records"
+    __tablename__ = "detection_results"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        index=True,
+    )
     timestamp = Column(DateTime, default=datetime.now)
     num_boxes = Column(Integer, nullable=False)
     image_path = Column(String, nullable=False)
