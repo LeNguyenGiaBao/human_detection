b9575018c2f0b962f444c16a6ed5ce2b1072cd35
lenguyengiabao <lenguyengiabao46@gmail.com>
2025-02-21 00:02:54 +0700

    feat(be): add CORS config

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,25 @@
+import os
+
 from app.infrastructure.database import init_db
 from app.infrastructure.response import ResponseFormatter
 from app.interfaces.controllers import router
 from constants import FAILED
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 init_db()
+load_dotenv()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.environ.get("FRONTEND_URL", "http://localhost:3000")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(RequestValidationError)
